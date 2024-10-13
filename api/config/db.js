0264bfcaf3ebac4136beab5a0d3a5771f0f954e4
Simon07012002 <alacmac71@gmail.com>
2024-10-13 19:41:11 -0400

    update

--- api/config/db.js
+++ api/config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        mongoose.set('strictQuery', false); // Set to false to suppress the warning
+        mongoose.set('strictQuery', false); 
         await mongoose.connect(process.env.MONGODB_URI, {
-            // No need for useNewUrlParser and useUnifiedTopology as they are deprecated
+  
         });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
-        process.exit(1); // Exit process with failure
+        process.exit(1); 
     }
 };
 
